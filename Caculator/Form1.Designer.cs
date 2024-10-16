48616e68ebb59c7455399da2234976a42642f01e
MinerHai <haiminer45@gmail.com>
2024-10-16 09:03:26 +0700

    .
    .

---
+++ Caculator/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Caculator
+{
+    partial class Caculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNum = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            btnTru = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            btnNhan = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            btnChia = new Button();
+            btnChiaDu = new Button();
+            btnClear = new Button();
+            button16 = new Button();
+            SuspendLayout();
+            // 
+            // txtNum
+            // 
+            txtNum.Font = new Font("Segoe UI", 20F);
+            txtNum.Location = new Point(25, 12);
+            txtNum.Name = "txtNum";
+            txtNum.Size = new Size(426, 43);
+            txtNum.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button1.Location = new Point(22, 77);
+            button1.Name = "button1";
+            button1.Size = new Size(82, 64);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button2.Location = new Point(139, 77);
+            button2.Name = "button2";
+            button2.Size = new Size(82, 64);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button3.Location = new Point(255, 77);
+            button3.Name = "button3";
+            button3.Size = new Size(82, 64);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button4.Location = new Point(369, 77);
+            button4.Name = "button4";
+            button4.Size = new Size(82, 64);
+            button4.TabIndex = 4;
+            button4.Text = "+";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // btnTru
+            // 
+            btnTru.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            btnTru.Location = new Point(369, 157);
+            btnTru.Name = "btnTru";
+            btnTru.Size = new Size(82, 64);
+            btnTru.TabIndex = 8;
+            btnTru.Text = "-";
+            btnTru.UseVisualStyleBackColor = true;
+            btnTru.Click += btnTru_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button6.Location = new Point(255, 157);
+            button6.Name = "button6";
+            button6.Size = new Size(82, 64);
+            button6.TabIndex = 7;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button7.Location = new Point(139, 157);
+            button7.Name = "button7";
+            button7.Size = new Size(82, 64);
+            button7.TabIndex = 6;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button8.Location = new Point(22, 157);
+            button8.Name = "button8";
+            button8.Size = new Size(82, 64);
+            button8.TabIndex = 5;
+            button8.Text = "4";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // btnNhan
+            // 
+            btnNhan.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            btnNhan.Location = new Point(369, 237);
+            btnNhan.Name = "btnNhan";
+            btnNhan.Size = new Size(82, 64);
+            btnNhan.TabIndex = 12;
+            btnNhan.Text = "*";
+            btnNhan.UseVisualStyleBackColor = true;
+            btnNhan.Click += btnNhan_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button10.Location = new Point(255, 237);
+            button10.Name = "button10";
+            button10.Size = new Size(82, 64);
+            button10.TabIndex = 11;
+            button10.Text = "9";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button11.Location = new Point(139, 237);
+            button11.Name = "button11";
+            button11.Size = new Size(82, 64);
+            button11.TabIndex = 10;
+            button11.Text = "8";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button12.Location = new Point(22, 237);
+            button12.Name = "button12";
+            button12.Size = new Size(82, 64);
+            button12.TabIndex = 9;
+            button12.Text = "7";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // btnChia
+            // 
+            btnChia.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            btnChia.Location = new Point(369, 320);
+            btnChia.Name = "btnChia";
+            btnChia.Size = new Size(82, 64);
+            btnChia.TabIndex = 16;
+            btnChia.Text = "/";
+            btnChia.UseVisualStyleBackColor = true;
+            btnChia.Click += btnChia_Click;
+            // 
+            // btnChiaDu
+            // 
+            btnChiaDu.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            btnChiaDu.Location = new Point(255, 320);
+            btnChiaDu.Name = "btnChiaDu";
+            btnChiaDu.Size = new Size(82, 64);
+            btnChiaDu.TabIndex = 15;
+            btnChiaDu.Text = "%";
+            btnChiaDu.UseVisualStyleBackColor = true;
+            btnChiaDu.Click += btnChiaDu_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            btnClear.Location = new Point(139, 320);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(82, 64);
+            btnClear.TabIndex = 14;
+            btnClear.Text = "C";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // button16
+            // 
+            button16.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold);
+            button16.Location = new Point(22, 320);
+            button16.Name = "button16";
+            button16.Size = new Size(82, 64);
+            button16.TabIndex = 13;
+            button16.Text = "=";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
+            // 
+            // Caculator
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(472, 399);
+            Controls.Add(btnChia);
+            Controls.Add(btnChiaDu);
+            Controls.Add(btnClear);
+            Controls.Add(button16);
+            Controls.Add(btnNhan);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(btnTru);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(txtNum);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Caculator";
+            Text = "Caculator";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNum;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button btnTru;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button btnNhan;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button btnChia;
+        private Button btnChiaDu;
+        private Button btnClear;
+        private Button button16;
+    }
+}
---
+++ Caculator/Form1.cs
@@ -0,0 +1,152 @@
+namespace Caculator
+{
+    public partial class Caculator : Form
+    {
+        public static double oldNum { set; get; }
+        public static double curNum { set; get; }
+        public static bool isoldNum { set; get; }
+        public static string? PhepTinh { set; get; }
+        public Caculator()
+        {
+            InitializeComponent();
+            isoldNum = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "7";
+            if (isoldNum) oldNum = 7;
+            else curNum = 7;
+     
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "1";
+            if (isoldNum) oldNum = 1;
+            else curNum = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "2";
+            if (isoldNum) oldNum = 2;
+            else curNum = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "3";
+            if (isoldNum) oldNum = 3;
+            else curNum = 3;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "4";
+            if (isoldNum) oldNum = 4;
+            else curNum = 4;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "5";
+            if (isoldNum) oldNum = 5;
+            else curNum = 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "6";
+            if (isoldNum) oldNum = 6;
+            else curNum = 6;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "8";
+            if (isoldNum) oldNum = 8;
+            else curNum = 8;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "9";
+            if (isoldNum) oldNum = 9;
+            else curNum = 9;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "";
+            oldNum = 0;
+            curNum = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        { 
+                PhepTinh = "Cong";
+                isoldNum = false;
+                txtNum.Text = "";
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            PhepTinh = "Tru";
+            isoldNum = false;
+            txtNum.Text = "";
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            PhepTinh = "Nhan";
+            isoldNum = false;
+            txtNum.Text = "";
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            PhepTinh = "Chia";
+            isoldNum = false;
+            txtNum.Text = "";
+        }
+
+        private void btnChiaDu_Click(object sender, EventArgs e)
+        {
+                PhepTinh = "ChiaDu";
+                isoldNum = false;
+                txtNum.Text = "";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            switch (PhepTinh)
+            {
+                case "Cong":
+                    oldNum = (oldNum + curNum);
+                    break;
+                case "Tru":
+                    oldNum = (oldNum - curNum);
+                    break;
+                case "Nhan":
+                    oldNum = (oldNum * curNum);
+                    break;
+                case "Chia":
+                    oldNum = (oldNum / curNum);
+                    break;
+                case "ChiaDu":
+                    oldNum = (oldNum % curNum);
+                    break;
+                default:
+                    break;
+            }
+            txtNum.Text = oldNum.ToString();
+            isoldNum = true;
+        }
+    }
+}
